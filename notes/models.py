2c2ed482d2c5fb59d9d19caf537b61db62f22ef2
Hendrilalaina <hendrilalaina@gmail.com>
2025-06-30 13:37:33 +0300

    Adding field likes to Note model

--- notes/models.py
+++ notes/models.py
@@ -3,4 +3,5 @@ from django.db import models
 class Note(models.Model):
     title = models.CharField(max_length=25)
     description = models.TextField()
+    likes = models.PositiveIntegerField(default=0)
     created = models.DateTimeField(auto_now_add=True)
