2d693c8076051185d3834cf5e3770d9f6b4308c5
Hendrilalaina <hendrilalaina@gmail.com>
2025-06-30 03:19:09 +0300

    Adding contact template

--- home/views.py
+++ home/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request, 'home/welcome.html', {})
+
+@login_required(login_url='/admin')
+def contact(request):
+    return render(request, 'home/contact.html', {})
