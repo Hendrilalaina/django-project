b835d35a2994c8ece6e1f6152840bc1d5842795d
Hendrilalaina <hendrilalaina@gmail.com>
2025-06-30 13:27:43 +0300

    Adding note model

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+class NotesAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description')
+
+admin.site.register(models.Note, NotesAdmin)
--- notes/models.py
+++ notes/models.py
@@ -1,1 +1,6 @@
+from django.db import models
 
+class Note(models.Model):
+    title = models.CharField(max_length=25)
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
