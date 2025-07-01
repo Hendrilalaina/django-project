cb3b82ff4e4175240ba06ddc83030b111a45b48f
Hendrilalaina <hendrilalaina@gmail.com>
2025-07-01 16:18:31 +0300

    Adding note details

--- notes/views.py
+++ notes/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.http import Http404
 
 from .models import Note
 
 def list_notes(request):
     all_notes = Note.objects.all
     return render(request, 'notes/notes_list.html', {'notes': all_notes})
+
+def detail_note(request, pk):
+    try:
+        note = Note.objects.get(pk=pk)
+    except Note.DoesNotExist:
+        raise Http404("Note does not exist")
+    return render(request, 'notes/note_details.html', {'note': note})
